a7cd92945e6ab449fd835e1cfb015388a83b8e76
KoKoLates <the21515@gmail.com>
2024-07-08 19:35:48 +0800

    chore(plot): update model and monitor
    * Line3D and Line2D issue within model dataclass should be dealt
    * check the position and attitude operation are correct

--- monitor/plot.py
+++ monitor/plot.py
@@ -6,11 +6,20 @@ from dataclasses import dataclass
 from mpl_toolkits.mplot3d.art3d import Line3D
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-from quad import rotation_matrix, State
+from quadcopter import rotation_matrix
+from quadcopter import Quadcopter
+
+
+@dataclass
+class Model(object):
+    l1: Line3D
+    l2: Line3D
+    node: Line3D
+    points: np.ndarray
 
 
 class Monitor(object):
-    def __init__(self) -> None:
+    def __init__(self, quad: Quadcopter) -> None:
         self.fig = plt.figure()
         self.ax = Axes3D(self.fig)
         self.ax.set_xlim3d([-2.0, 2.0])
@@ -21,15 +30,27 @@ class Monitor(object):
         self.ax.set_zlabel("Z")
         self.ax.set_title("Quadcopter Simulation")
 
-        
+        self.quad: Quadcopter = quad
+        self.load_model()
+
     def load_model(self) -> None:
         line1, _ = self.ax.plot([], [], [], color="b", linewidth=3, antialiased=False)
-        self.model: Model = Model(line1, line1, line1, np.array([]))
+        line2, _ = self.ax.plot([], [], [], color="r", linewidth=3, antialiased=False)
+        line3, _ = self.ax.plot([], [], [], color="r", linewidth=3, antialiased=False)
+        
+        L: float = self.quad.l
+        point: np.ndarray = np.array([
+            [-L, 0, 0], [ L, 0, 0], [ 0,-L, 0], 
+            [ 0, L, 0], [ 0, 0, 0], [ 0, 0, 0]
+        ]).T
+        
+        self.model: Model = Model(line1, line2, line3, point)
 
-    def update(self, state: State) -> None:
-        R: np.ndarray = rotation_matrix(state.o)
+    def update(self) -> None:
+        position, _, attitude, _ = self.quad.state
+        R: np.ndarray = rotation_matrix(attitude)
         points = R @ self.model.points
-        points += state.p[:, np.newaxis]
+        points += position[:, np.newaxis]
 
         self.model.l1.set_data(points[0, 0:2], points[1:0, 2])
         self.model.l1.set_3d_properties(points[2, 0:2])
@@ -38,13 +59,4 @@ class Monitor(object):
         self.model.node.set_data(points[0, 5], points[1, 5])
         self.model.node.set_3d_properties(points[2, 5])
 
-        plt.pause(1E-5)
-
-
-@dataclass
-class Model(object):
-    l1: Line3D = None
-    l2: Line3D = None
-    node: Line3D = None
-
-    points: np.ndarray = None
+        plt.pause(1e-5)
