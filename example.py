79dc7dfa9eb79af262b1e2138fe39e1471db0948
KoKoLates <the21515@gmail.com>
2024-11-27 01:56:38 +0800

    feat(example): waypoints loader prototype
    - simulation loop with simulator and waypoint loader
    - check the target reaching mechanism latter to get into next waypoint
      tracking step
    - input datatype checking should be implemented later

--- example.py
+++ example.py
@@ -4,30 +4,52 @@ from quad import *
 from simulator import Simulator
 
 
-def main() -> None:
-    quad_config = QuadConfig(
-        10, 10, 1, [0, 0, 0], 
-        MotorConfig(10, 10, 0, 0), 10
-    )
-    ctrl_config = ControlConfig(
+class WaypointLoader(object):
+    def __init__(self, waypoints: list[tuple]) -> None:
+        ## can add some check here, like:
+        # 1. tuple format with (x, y, z, yaw)
+        # 2. yaw and z limit, z most be non-negative
+        self.waypoints: list[tuple] = waypoints
+        self.index: int = 0
 
+
+def main() -> None:
+    waypoints: list[tuple] = [
+        (1, 1, 2, 0),
+        (1, -1, 4, 0),
+        (-1, -1, 2, 0),
+        (-1, 1, 4, 0),
+    ]
+
+    quad_config = QuadConfig(
+        weight=1.2,
+        length=0.3,
+        radius=0.1,
+        states=[[0, 0, 0], [0, 0, 0]],
+        motors=MotorConfig(10, 4.5, 0, 0),
+        drag_c=0.1,
     )
+    quad: Quadcopter = Quadcopter(quad_config)
+    app: Simulator = Simulator(quad_config)
 
     quad = Quadcopter(quad_config)
-    ctrl = Controller(ctrl_config)
+    ctrl = Controller()
 
-    waypoints = (1, 1, 1, 0)
     quad.start()
-    ctrl.start() 
+    ctrl.start()
 
     try:
         while True:
-            ctrl.update_target(waypoints)
+            for target in waypoints:
+                ctrl.update_target(target)
+                # keep update and wait reach
+                app.update(quad.states)
 
     except KeyboardInterrupt:
         quad.stop()
         ctrl.stop()
         print("Simulation Stopped.")
+
 
 if __name__ == "__main__":
     main()
